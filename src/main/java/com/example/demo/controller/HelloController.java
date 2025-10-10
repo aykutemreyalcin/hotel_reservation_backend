a5365d8effd8bc6f4d732f50bc314ac1babdef5b
aykut <aykutemyeyalcinn@gmail.com>
2025-10-10 14:29:20 +0200

    test controller

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class HelloController {
 
-    @
+    @GetMapping
+    public String hello() {
+        return "hello";
+    }
 }
